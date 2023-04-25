f45a5e9477abde2c58923d9fae92e5933949af2d
realdashiedash <31542077+dashiedash@users.noreply.github.com>
2023-04-25 13:11:13 +0800

    Set API version routing

--- routes/api.php
+++ routes/api.php
@@ -18,5 +18,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-// API
-
+Route::group(['prefix' => 'v1', 'namespace' => 'App\Http\Controllers\Api\V1'], function () {
+    Route::apiResource('customers', CustomerController::class);
+    Route::apiResource('invoices', InvoiceController::class);
+});
