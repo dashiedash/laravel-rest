68a91d490dd939d4cb6fd5c8f9314adc8438bce3
realdashiedash <31542077+dashiedash@users.noreply.github.com>
2023-04-26 13:52:02 +0800

    Rename bulk request files
    Rename BulkStoreCustomerRequest to BulkStoreInvoiceRequest.

--- app/Http/Requests/V1/BulkStoreInvoiceRequest.php
+++ app/Http/Requests/V1/BulkStoreInvoiceRequest.php
@@ -5,7 +5,7 @@ namespace App\Http\Requests\V1;
 use Illuminate\Validation\Rule;
 use Illuminate\Foundation\Http\FormRequest;
 
-class StoreCustomerRequest extends FormRequest
+class BulkStoreInvoiceRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
@@ -26,17 +26,23 @@ class StoreCustomerRequest extends FormRequest
             '*.customerId' => ['required', 'integer'],
             '*.amount' => ['required', 'numeric'],
             '*.status' => ['required', Rule::in(['B', 'P', 'V', 'b', 'p', 'v'])],
-            '*.billedDate' => ['required', 'date_format:Y-m-d H:i:s
-            '],
-            '*.paidDate' => ['date_format:Y-m-d H:i:s
-            ', 'nullable'],
+            '*.billedDate' => ['required', 'date_format:Y-m-d H:i:s'],
+            '*.paidDate' => ['date_format:Y-m-d H:i:s', 'nullable'],
         ];
     }
 
     protected function prepareForValidation()
     {
-        $this->merge([
-            'postal_code' => $this->postalCode
-        ]);
+        $data = [];
+
+        foreach ($this->toArray() as $obj) {
+            $obj['customer_id'] = $obj['customerId'] ?? null;
+            $obj['billed_date'] = $obj['billedDate'] ?? null;
+            $obj['paid_date'] = $obj['paidDate'] ?? null;
+
+            $data[] = $obj;
+        }
+
+        $this->merge($data);
     }
 }
