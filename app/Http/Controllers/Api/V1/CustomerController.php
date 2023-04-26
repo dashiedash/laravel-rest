f0e2135b1887ec56898a77a524a26c8c2fb20b8d
realdashiedash <31542077+dashiedash@users.noreply.github.com>
2023-04-26 13:12:20 +0800

    Set PUT and PATCH validation and rules

--- app/Http/Controllers/Api/V1/CustomerController.php
+++ app/Http/Controllers/Api/V1/CustomerController.php
@@ -7,9 +7,9 @@ use Illuminate\Http\Request;
 use App\Filters\V1\CustomersFilter;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\V1\CustomerResource;
-use App\Http\Requests\UpdateCustomerRequest;
 use App\Http\Resources\V1\CustomerCollection;
 use App\Http\Requests\V1\StoreCustomerRequest;
+use App\Http\Requests\V1\UpdateCustomerRequest;
 
 class CustomerController extends Controller
 {
@@ -60,7 +60,7 @@ class CustomerController extends Controller
      */
     public function update(UpdateCustomerRequest $request, Customer $customer)
     {
-        //
+        $customer->update($request->all());
     }
 
     /**
