0f774927a8309b29cfef8a3ffe3fa0b0cf49bb64
realdashiedash <31542077+dashiedash@users.noreply.github.com>
2023-04-26 10:28:37 +0800

    Enable store fuction

--- app/Http/Controllers/Api/V1/CustomerController.php
+++ app/Http/Controllers/Api/V1/CustomerController.php
@@ -37,7 +37,7 @@ class CustomerController extends Controller
      */
     public function store(StoreCustomerRequest $request)
     {
-        //
+        return new CustomerResource(Customer::create($request->all()));
     }
 
     /**
